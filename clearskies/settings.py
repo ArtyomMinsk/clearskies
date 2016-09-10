9fe240d31cf997b06fe37b88dadc10565c72ee56
Peter Lawless <peterlawless223@gmail.com>
2016-09-10 13:50:36 -0400

    Started clearskies_app

--- clearskies/settings.py
+++ clearskies/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'clearskies_app.apps.ClearskiesAppConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
