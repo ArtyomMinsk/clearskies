f62d1527cd00c67ac488ad7dbcdc274a6b4d9309
Peter Lawless <peterlawless223@gmail.com>
2016-09-10 11:56:14 -0400

    Recursively removed .direnv file

--- .direnv/python-3.5.1/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/peterlawless/Documents/tiy/week10/clearskies/.direnv/python-3.5.1/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
