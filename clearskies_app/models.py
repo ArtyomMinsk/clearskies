efb117917728df6559a0109cc954e29650adbd18
Peter Lawless <peterlawless223@gmail.com>
2016-09-11 15:10:09 -0400

    Created model for Airfield

--- clearskies_app/models.py
+++ clearskies_app/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Airfield(models.Model):
+    identifier = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
