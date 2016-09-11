68f292bc8f050d52f98cae7354240c018e78f24c
T. Robert Ward <trironyard@icloud.com>
2016-09-10 20:03:44 -0400

    tweaked scraper

--- clearskies_app/scrape_data.py
+++ clearskies_app/scrape_data.py
@@ -1,9 +1,16 @@
 import requests
 
-url = "https://www.aviationweather.gov/metar/data?ids=KSOP&format=raw&hours=0&taf=off&layout=on&date=0"
+url = "https://www.aviationweather.gov/metar/data?ids=KLAX&format=raw&hours=0&taf=off&layout=on&date=0"
 res = requests.get(url)
 text = res.text
-find_me = "<!-- Data starts here -->"
-beg_position_of_data = text.find(find_me) + 25
-print(beg_position_of_data)
-print(text[beg_position_of_data:beg_position_of_data + 50])
+find_beg = "<!-- Data starts here -->"
+find_end = "<br /><hr"
+beg_position_of_data = text.find(find_beg) + 25
+end_position_of_data = text.find(find_end)
+print(text[beg_position_of_data:end_position_of_data])
+
+
+# break string up:
+# first_part="https://www.aviationweather.gov/metar/data?ids="
+# third_part="&format=raw&hours=0&taf=off&layout=on&date=0"
+# middle_part  = " pass it an airport DUDE "
