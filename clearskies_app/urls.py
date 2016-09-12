42d7d3438d45ea1bfe615b5638470753481b84e5
Peter Lawless <peterlawless223@gmail.com>
2016-09-12 17:33:20 -0400

    Created proof-of-concept view 'plan' to make sure that database can be queried via html form

--- clearskies_app/urls.py
+++ clearskies_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'clearskies_app'
 urlpatterns = [
+    url(r'^plan/$', views.plan, name="plan"),
     url(r'', views.home, name="home"),
+
 ]
--- clearskies_app/views.py
+++ clearskies_app/views.py
@@ -5,9 +5,22 @@ from .models import Airfield
 def home(request):
     return render(request, 'clearskies_app/index.html', context=None)
 
+
 def cloud_bases_report(request):
     pass
     # print the list of
-    #DEF  get coords of start and finish from DB
-    #DEF  calc the rectangle
-    #DEF  get all the stations within the LAT LON rectangle
+    # DEF  get coords of start and finish from DB
+    # DEF  calc the rectangle
+    # DEF  get all the stations within the LAT LON rectangle
+
+
+def plan(request):
+    if request.method == "POST":
+        print(request.POST)
+        start = Airfield.objects.get(identifier=request.POST['start'])
+        finish = Airfield.objects.get(identifier=request.POST['finish'])
+    else:
+        start = ''
+        finish = ''
+    return render(request, 'clearskies_app/plan.html', {'start': start,
+                                                        'finish': finish})
