f6453099342a55ca02143e8abd45bd53a972c0da
T. Robert Ward <trironyard@icloud.com>
2016-09-14 13:26:09 -0400

    created flight path markers in google maps API

--- clearskies_app/urls.py
+++ clearskies_app/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 app_name = 'clearskies_app'
 urlpatterns = [
-    url(r'^get_coord/$', views.coord, name="coord"),
-    url(r'^plan/$', views.plan, name="plan"),
+    url(r'^get_coord/$', views.get_coord, name="coord"),
+    url(r'^plan/$', views.get_corridor_airports, name="plan"),
+    url(r'^Tindex/$', views.Tindex, name="Tindex"),
     url(r'', views.home, name="home"),
 
 ]
