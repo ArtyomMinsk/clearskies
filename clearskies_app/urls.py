43690e7e509274920064a5617bad2af481677684
T. Robert Ward <trironyard@icloud.com>
2016-09-15 12:05:40 -0400

    merge views

--- clearskies_app/urls.py
+++ clearskies_app/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 app_name = 'clearskies_app'
 urlpatterns = [
-    url(r'^Tindex/$', views.Tindex, name="Tindex"),
+    # url(r'^Tindex/$', views.Tindex, name="Tindex"),
     url(r'^get_coord/$', views.coord, name="coord"),
-    url(r'^plan/$', views.plan, name="plan"),
+    # url(r'^plan/$', views.plan, name="plan"),
     url(r'^test/$', views.legs, name="legs"),
     url(r'', views.home, name="home"),
 ]
