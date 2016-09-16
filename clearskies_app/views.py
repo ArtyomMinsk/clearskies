a6447c257bf37e78263f692beed5d677c6f641fd
Peter Lawless <peterlawless223@gmail.com>
2016-09-15 20:59:35 -0400

    Created a class to parse the data from a METAR string

--- clearskies_app/views.py
+++ clearskies_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Airfield
+from .models import Airfield, METAR
 from numpy import arange
 import requests
 
