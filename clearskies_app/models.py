f9808cd2e813001c7189cc207266def44384992c
Peter Lawless <peterlawless223@gmail.com>
2016-09-19 14:10:39 -0400

    Test

--- clearskies_app/models.py
+++ clearskies_app/models.py
@@ -64,6 +64,9 @@ class METAR:
 
     def __eq__(self, other):
         return self.identifier == other.identifier
+
+    #HELLO
+
     #
     # def parse_windspeed(self, item):
     #     self.windbearing = item[:3]
