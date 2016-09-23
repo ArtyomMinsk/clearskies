c1b3e8bc3cfd8a30be6a23cf93d6abda3c70fb1d
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-09-23 17:17:54 -0400

    Added the version of the page with the tabs

---
+++ clearskies_app/static/js/tabs.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = $(this).attr('href');
+
+        // Show/Hide Tabs
+        // $('#tabs ' + currentAttrValue).show().siblings().hide();
+        $('#tabs ' + currentAttrValue).slideDown(400).siblings().slideUp(400);
+
+        // Change/remove current tab to active
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+    });
+});
