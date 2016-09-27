3b19fb0b8b55b6bcd445e2b3db994312f75342ce
T. Robert Ward <trironyard@icloud.com>
2016-09-27 10:51:37 -0400

    deleted all unused files

--- API_stuff/geoCoder.py
+++
@@ -1,9 +0,0 @@
-import urllib2
-
-airport="KRDU"
-address="1600+Amphitheatre+Parkway,+Mountain+View,+CA"
-key="AIzaSyBJRBQ0vUyLij18oHW8A_mwdsJbyYnlDUk"
-url="https://maps.googleapis.com/maps/api/geocode/json?airport=%s&key=%s" % (airport, key)
-
-response = urllib2.urlopen(url)
-jsongeocode = response.read()
--- unused_BUT_dont_delete_yet/get_CB_data3.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://www.aviationweather.gov/metar/data?ids=KSOP&format=raw&hours=0&taf=off&layout=on&date=0"
-res = requests.get(url)
-text = res.text
-find_me = "<!-- Data starts here -->"
-beg_position_of_data = text.find(find_me) + 25
-print(beg_position_of_data)
-print(text[beg_position_of_data:beg_position_of_data + 50])
