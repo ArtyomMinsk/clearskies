56c4939e979fd496af3ce327918e8f3616ce12ad
Peter Lawless <peterlawless223@gmail.com>
2016-09-27 14:46:04 -0400

    Fixed loophole in legs view that made duplicate data for middle waypoints

--- clearskies_app/views.py
+++ clearskies_app/views.py
@@ -55,7 +55,6 @@ def get_corridor_airports(st, fin, width):
     if lon_diff > lat_diff:
         ratio = lat_diff / (lon_diff * 10)
         step_thru = "lon"
-        # avg_stations = round(lon_diff)
         if startLON < finishLON:
             increment = 0.1
             extend = 0.4
@@ -65,7 +64,6 @@ def get_corridor_airports(st, fin, width):
     else:
         ratio = lon_diff / (lat_diff * 10)
         step_thru = "lat"
-        # avg_stations = round(lat_diff)
         if startLAT < finishLAT:
             increment = 0.1
             extend = 0.4
@@ -81,7 +79,6 @@ def get_corridor_airports(st, fin, width):
         finishP = finishLAT
 
     for i in arange(startP, finishP + extend, increment):
-        # print(i, "<====== THIS IS I")
         if step_thru == 'lon':
             for each_airport in selected_airports:
                 if each_airport.latitude <= startLAT + width and each_airport.latitude >= startLAT - width and each_airport.longitude <= i and each_airport.longitude >= i - 0.1:
@@ -92,16 +89,13 @@ def get_corridor_airports(st, fin, width):
                 startLAT -= ratio
             else:
                 startLAT += ratio
-            # print('startLAT', startLAT)
 
         elif step_thru == 'lat':
             for each_airport in selected_airports:
-                # print('each_airport.longitude', each_airport.longitude, 'startLON', startLON, startLON+0.4)
                 if each_airport.longitude <= startLON + width and each_airport.longitude >= startLON - width and each_airport.latitude <= i and each_airport.latitude >= i - 0.1:
                     wx = get_data(each_airport.identifier)
                     if wx:
                         airport_weather.append((each_airport, METAR(wx)))
-            # print("now I'm at the incr startLON - ratio")
             if startLON > finishLON:
                 startLON -= ratio
             else:
@@ -118,7 +112,7 @@ def get_corridor_airports(st, fin, width):
 
 
 # this function gets the all airports in the whole flight path
-@cache_page(60 * 0)
+# @cache_page(60 * 0)
 def legs(request):
     weather_stations = []
     identifiers = request.GET.getlist('waypoint')
@@ -126,9 +120,13 @@ def legs(request):
 
     for i in range(len(identifiers)):
         if (i + 1) != len(identifiers):
-            start_time = time.time()
-            weather_list = get_corridor_airports(identifiers[i], identifiers[i + 1], float(corr_width))
+            weather_list = get_corridor_airports(identifiers[i],
+                                                 identifiers[i + 1],
+                                                 float(corr_width))
+        if i == 0:
             weather_stations += weather_list
+        else:
+            weather_stations += weather_list[1:]
 
     full_list = []
     for item in weather_stations:
@@ -140,7 +138,6 @@ def legs(request):
                      "longitude": item[0].longitude,
                      "ceiling": item[1].ceiling}
         full_list.append(datapoint)
-
 
     return JsonResponse(full_list, safe=False)
 
@@ -160,21 +157,14 @@ def get_data(AI):
     return text[beg_position_of_data:end_position_of_data]
 
 
-# Delete this when done testing
 def instant_plot(request):
     if request.method == "GET":
-        # print("IT IS A GET REQUEST!!!---------------------->", request.GET)
-        # temp = Airfield.objects.get(identifier=request.GET['airportID'])
         temp = get_object_or_404(Airfield, identifier=request.GET['airportID'])
-        # plotLAT = temp.latitude
-        # plotLON = temp.longitude
         airfield = {'latitude': temp.latitude, 'longitude': temp.longitude,
                     'name': temp.name, 'city': temp.city, 'state': temp.state,
                     'identifier': temp.identifier}
     else:
         airfield = {}
-    # context = {'lat': plotLAT, 'lon': plotLON}
-    # return it to HTML - so it goes on G Map API instantaneous !!!!!!!!
     return JsonResponse(airfield)
 
 
